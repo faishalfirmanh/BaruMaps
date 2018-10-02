0a4947a134ac28690b73f92d316d79280794917f
isal48 <isalredarmy1@gmail.com>
2018-10-02 22:18:35 +0700

    hapus hitng jarak

--- src/pages/jarak/jarak.ts
+++ src/pages/jarak/jarak.ts
@@ -23,18 +23,13 @@ export class JarakPage {
     public todos=[];
       name: string;
 
-  @ViewChild('map') mapElement: ElementRef;
-  map: any;
-  start = 'chicago, il';
-  end = 'chicago, il';
-  directionsService = new google.maps.DirectionsService;
-  directionsDisplay = new google.maps.DirectionsRenderer;
+
 
   constructor(public navCtrl: NavController, public navParams: NavParams,public geolocation: Geolocation, public trackingService: TrackingService) {
-    this.getUsers();
+
   }
   ionViewDidLoad(){
-    this.initMap();
+
   }
 
   initMap() {
@@ -56,40 +51,16 @@ export class JarakPage {
 
 
 
+    // this.map = new google.maps.Map(this.mapElement.nativeElement, {
+    //
+    //   zoom: 7,
+    //    center: {lat: 45, lng: -87.65}
+    // });
 
-
-    this.map = new google.maps.Map(this.mapElement.nativeElement, {
-
-      zoom: 7,
-       center: {lat: 45, lng: -87.65}
-    });
-
-    this.directionsDisplay.setMap(this.map);
 
 
   }
 
-  calculateAndDisplayRoute() {
-    this.directionsService.route({
-      origin: this.start,
-      destination: this.end,
-      travelMode: 'DRIVING'
-    }, (response, status) => {
-      if (status === 'OK') {
-        this.directionsDisplay.setDirections(response);
-      } else {
-        window.alert('Directions request failed due to ' + status);
-      }
-    });
 
-  }
-
-  getUsers() {
-this.trackingService.getUsers()
-.then(data => {
-  this.users = data;
-  console.log(this.users);
-});
-}
 
 }
